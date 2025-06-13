8a3e43daa7d9f96d99595d6fd22f9009898cb797
Daichi Toyohara <daichi.t.toyohara@avanade.com>
2025-06-13 13:35:12 +0900

    fix: supabaseからuser情報を取得し表示

--- src/app/components/ui/user-information.tsx
+++ src/app/components/ui/user-information.tsx
@@ -3,33 +3,46 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./dropdown-menu";
-import { LogOut, Settings, User } from "lucide-react";
+import { LogOut } from "lucide-react";
+import useAuth from "@/app/hooks/use-auth";
 
 export default function UserInformation() {
+  const { session, signOut } = useAuth();
+
+  const avatarUrl = session?.user?.user_metadata?.avatar_url;
+  const userName = session?.user?.user_metadata?.user_name;
+  const userEmail = session?.user?.email;
+
   return (
     <div className="ml-auto">
       <DropdownMenu>
-        <DropdownMenuTrigger className="focus:outline-none focus:ring-[2px] focus:ring-offset-2 focus:ring-primary rounded-full">
+        <DropdownMenuTrigger className="flex items-center gap-3">
           <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
+            <AvatarImage src={avatarUrl} alt={userName} />
+            <AvatarFallback className="bg-primary text-primary-foreground">
+              {userName?.charAt(0)}
+            </AvatarFallback>
           </Avatar>
         </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuContent className="mt-2 w-72">
+          <DropdownMenuItem className="py-3">
+            <Avatar>
+              <AvatarImage src={avatarUrl} alt={userName} />
+              <AvatarFallback className="bg-primary text-primary-foreground">
+                {userName?.charAt(0)}
+              </AvatarFallback>
+            </Avatar>
+            <div className="ml-1 flex flex-col">
+              <p className="text-sm font-medium">{userName}</p>
+              <p className="text-xs text-muted-foreground">{userEmail}</p>
+            </div>
+          </DropdownMenuItem>
           <DropdownMenuSeparator />
           <DropdownMenuItem>
-            <User className="h-4 w-4" /> Profile
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Settings className="h-4 w-4" /> Settings
-          </DropdownMenuItem>
-          <DropdownMenuItem className="text-destructive">
-            <LogOut className="h-4 w-4" /> Logout
+            <LogOut className="mr-1" onClick={signOut} /> Sign out
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
--- src/app/hooks/use-auth.ts
+++ src/app/hooks/use-auth.ts
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { Session } from "@supabase/supabase-js";
 import { supabase } from "../lib/supabase-client";
 
-export default function useUser() {
+export default function useAuth() {
   const [session, setSession] = useState<Session | null | undefined>(undefined);
 
   useEffect(() => {
@@ -29,6 +29,8 @@ export default function useUser() {
   function signOut() {
     supabase.auth.signOut();
   }
+
+  console.log("useAuth session:", session?.user);
 
   return {
     session,
