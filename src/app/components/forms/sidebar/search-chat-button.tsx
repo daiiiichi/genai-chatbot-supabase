e389a54a1170c0c69c089b424a287c78f1308ac7
Daichi Toyohara <daichi.t.toyohara@avanade.com>
2025-06-18 15:48:32 +0900

    add: チャットの検索機能の追加（タイトルでの絞り込みのみ）

--- src/app/components/forms/sidebar/search-chat-button.tsx
+++ src/app/components/forms/sidebar/search-chat-button.tsx
@@ -1,15 +1,67 @@
+"use client";
+
+import { useState } from "react";
 import { SidebarMenuButton, SidebarMenuItem } from "../../ui/sidebar";
 import { Search } from "lucide-react";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "../../ui/command";
+import { useAtomValue, useSetAtom } from "jotai";
+import {
+  chatHistoriesAtom,
+  currentChatIdAtom,
+  messagesAtom,
+} from "@/app/atoms/chat";
+import { selectChat } from "@/app/lib/chat";
 
 export default function SearchChatButton() {
+  const [open, setOpen] = useState(false);
+  const chatHistories = useAtomValue(chatHistoriesAtom);
+  const setMessages = useSetAtom(messagesAtom);
+  const setCurrentChatId = useSetAtom(currentChatIdAtom);
+
   return (
     <SidebarMenuItem>
       <SidebarMenuButton asChild>
-        <a>
+        <a onClick={() => setOpen(true)}>
           <Search />
           <span>Search Chat</span>
         </a>
       </SidebarMenuButton>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="chat">
+            {chatHistories
+              .sort(
+                (a, b) =>
+                  new Date(b.updated_at).getTime() -
+                  new Date(a.updated_at).getTime()
+              )
+              .map((data) => (
+                <CommandItem
+                  key={data.chat_session_id}
+                  onSelect={() => {
+                    selectChat(
+                      data.chat_session_id,
+                      setCurrentChatId,
+                      setMessages
+                    );
+                    setOpen(false);
+                  }}
+                >
+                  <span>{data.title}</span>
+                </CommandItem>
+              ))}
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
     </SidebarMenuItem>
   );
 }
