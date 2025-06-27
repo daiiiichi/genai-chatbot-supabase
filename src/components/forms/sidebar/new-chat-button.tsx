ed43b2e113f8665da837b0ef21ab47dc60769f4c
Daichi Toyohara <daichi.t.toyohara@avanade.com>
2025-06-27 15:11:33 +0900

    fix: ルーティング漏れ修正

--- src/components/forms/sidebar/new-chat-button.tsx
+++ src/components/forms/sidebar/new-chat-button.tsx
@@ -9,16 +9,19 @@ import {
   userIdAtom,
 } from "@/atoms";
 import { DEFAULT_LLM_MODEL } from "@/constants/llm-model-list";
+import { useRouter } from "next/navigation";
 
 export default function NewChatButton() {
   const userId = useAtomValue(userIdAtom);
   const setMessages = useSetAtom(messagesAtom);
   const setCurrentChatId = useSetAtom(currentChatIdAtom);
   const SetLlmModel = useSetAtom(llmModelAtom);
+  const router = useRouter();
 
   const handleNewChat = async () => {
     const newChat = await startNewChat(userId);
     if (newChat) {
+      router.push(`/?chatId=${newChat.chatSessionId}`);
       setMessages(newChat.messages);
       setCurrentChatId(newChat.chatSessionId);
       SetLlmModel(DEFAULT_LLM_MODEL);
