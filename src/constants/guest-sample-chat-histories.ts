ce36be8588d110540115df6562827a1ec3a120cd
Daichi Toyohara <daichi.t.toyohara@avanade.com>
2025-06-30 13:22:24 +0900

    add: 会話履歴のサンプルを追加

---
+++ src/constants/guest-sample-chat-histories.ts
@@ -0,0 +1,14 @@
+export const sampleChatHistories = [
+  {
+    chat_session_id: "dabc30fb-3c48-4192-b07e-abd59274c2cb",
+    created_at: "2025-06-26 05:38:46.599+00",
+    title: "Reactの概要",
+    updated_at: "2025-06-26 05:50:13.167+00",
+  },
+  {
+    chat_session_id: "126c4cf9-d058-4b7d-a59a-a20f75e41b28",
+    created_at: "2025-06-27 05:38:46.599+00",
+    title: "カレーのレシピ",
+    updated_at: "2025-06-28 05:50:13.167+00",
+  },
+];
